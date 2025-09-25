f6e99b6717ce3befc4dd58dbbb3547d8cef9f11e
LinguaAnalytica <eddie@edwardgetman.com>
2025-09-25 06:08:58 -0400

    3a_output_formatting

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,38 @@
 import requests # Import the requests library to handle HTTP requests
+import json # Import the json library
+
 def emotion_detector(text_to_analyze): # Define a function named emotion_detector that takes a string input (text_to_analyze)
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict' # URL of the emotion detection service
     myobj = { "raw_document": { "text": text_to_analyze } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text  # Return the response text from the API
+    formatted_response = json.loads(response.text) # Parse the JSON response from the API
+    
+    # Extract emotion scores from the response
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger'] 
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    
+    # Identify the dominant emotion
+    emotion_scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    
+    # Return a dictionary containing emotion detection results
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }  
 
 
